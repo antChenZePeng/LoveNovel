72ac7118b17f5294246a800c83ebc253403e97cf
antChenZePeng <214957011@qq.com>
2019-02-14 17:56:40 +0800

    project init

--- src/main/java/com/wenzhi/novel/controller/TestController.java
+++ src/main/java/com/wenzhi/novel/controller/TestController.java
@@ -1,4 +1,16 @@
 package com.wenzhi.novel.controller;
 
+import com.wenzhi.novel.util.ResponseTool;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @RequestMapping(value = "/test.do", method = RequestMethod.GET)
+    public ResponseTool test(){
+        return ResponseTool.success(1);
+    }
+
 }
